910201747a4b87fdc35e0b5429053a1f587d0fc3
irakli abashidze <irakli.abashidzeee@gmail.com>
2024-01-23 23:07:40 +0400

    fixed carousel function and added timeout function

--- script.js
+++ script.js
@@ -9,38 +9,49 @@ document.addEventListener("scroll", () => {
     header.classList.remove("scrolled");
   }
 });
-
 // Carousel function
 
-let slideIndex = 1;
-showSlides(slideIndex);
+let slideIndex = 0;
+let slideTimeout; // Variable to store the timeout ID
 
-// Next/previous controls
+showSlides();
+
+// Next and previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
 
-// Thumbnail image controls
+// Dot controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
 
-function showSlides(n) {
+function showSlides() {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {
-    slideIndex = 1;
-  }
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
+
+  // Hide all slides
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
+
+  // Increment or decrement slideIndex, and handle wrap-around
+  slideIndex = (slideIndex + slides.length) % slides.length;
+
+  // Display the current slide and add "active" class to the corresponding dot
+  slides[slideIndex].style.display = "block";
+
+  // Remove "active" class from all dots
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+
+  dots[slideIndex].className += " active";
+
+  // Clear existing timeout and set a new one
+  clearTimeout(slideTimeout);
+  slideTimeout = setTimeout(function () {
+    plusSlides(1); // Change image every 5 seconds
+  }, 5000);
 }
